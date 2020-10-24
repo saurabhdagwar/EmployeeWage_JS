00535ccbd48b130150bdefc49e875803e3a35594
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-24 23:25:54 +0530

    add: calculate daily employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,3 +1,5 @@
+const wagePerHr = 20;
+const hrsInDay = 8;
 var empCheck = Math.floor(Math.random() * 2);
 if(empCheck == 1){
     console.log("Employee is Present ");
@@ -5,3 +7,5 @@ if(empCheck == 1){
 else{
     console.log("Employee is Absent ");
 }
+var dailyWage = wagePerHr * hrsInDay * empCheck;
+console.log("Daily Employee Wage : " + dailyWage);
