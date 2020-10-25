f1eb6a8de33a8cfff37ce71fc12407a94f9e8c86
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-25 21:40:20 +0530

    add: maxamium working hrs in a month is 100

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,13 @@
 const WAGE_PER_HR = 20;
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
+const MAX_MONTHLY_HRS = 100;
 const TOTAL_MONTHLY_DAYS = 20;
 var hrsInDay;
 var monthlyWage = 0;
-for(var day = 0; day < TOTAL_MONTHLY_DAYS; day++ ){
+var totalHrs = 0;
+var day = 0;
+for(var day =0;day < TOTAL_MONTHLY_DAYS;day ++){
     var empCheck = Math.floor(Math.random() * 3);
     switch(empCheck){
         case IS_FULL_TIME: 
@@ -17,7 +20,11 @@ for(var day = 0; day < TOTAL_MONTHLY_DAYS; day++ ){
             hrsInDay = 0;
             break;
     }
+    if(totalHrs >= 100 ){
+        break;
+    }
 var dailyWage = WAGE_PER_HR * hrsInDay;
 monthlyWage = monthlyWage + dailyWage;
+totalHrs = totalHrs + hrsInDay;
 }
 console.log("Monthly Employee Wage : " + monthlyWage);
