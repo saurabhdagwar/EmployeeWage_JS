172bbcd951ad35cf579bf31916805ff286b3cf84
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-25 10:52:22 +0530

    fix: code according with monthly wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,8 +4,8 @@ const IS_PART_TIME = 2;
 const TOTAL_MONTHLY_DAYS = 20;
 var hrsInDay;
 var monthlyWage = 0;
-var empCheck = Math.floor(Math.random() * 3);
 for(var day = 0; day < TOTAL_MONTHLY_DAYS; day++ ){
+    var empCheck = Math.floor(Math.random() * 3);
     switch(empCheck){
         case IS_FULL_TIME: 
             hrsInDay = 8;
